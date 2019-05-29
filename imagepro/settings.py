de131e03f7477f41cfcff375a0b6a7929e588bbc
GanapathiAmbore <50941599+GanapathiAmbore@users.noreply.github.com>
2019-05-29 11:00:25 +0530

    Update settings.py

--- imagepro/settings.py
+++ imagepro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't6xfygzo3ia_rb&mhq42l5zo+_71odn^00o)^dka#f+wf=6sg3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
